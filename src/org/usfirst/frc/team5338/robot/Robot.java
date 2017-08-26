faff64adff8f884436bf03b35c6aa5e31e15f7ec
ramanhariharan <ramanhariharan@10.130.224.81>
2017-08-26 16:08:41 -0400

    Adding autonomous

--- src/org/usfirst/frc/team5338/robot/Robot.java
+++ src/org/usfirst/frc/team5338/robot/Robot.java
@@ -3,16 +3,19 @@ package org.usfirst.frc.team5338.robot;
 import org.usfirst.frc.team5338.robot.subsystems.DriveTrain;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
+import org.usfirst.frc.team5338.robot.commands.*;
 
 public class Robot extends IterativeRobot {
 	public static final OI oi = new OI();
 	public static final DriveTrain drivetrain = new DriveTrain();
-
+	public static final Command autonomous = new autonomous();
 	public void robotInit() {
 	}
 
 	public void autonomousInit() {
+			autonomous.start();
 	}
 
 	public void autonomousPeriodic() {
@@ -20,6 +23,10 @@ public class Robot extends IterativeRobot {
 	}
 
 	public void teleopInit() {
+		try {
+			autonomous.cancel();
+		} catch (Exception e) { 
+		}
 	}
 
 	@Override
